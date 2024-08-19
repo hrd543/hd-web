89b1f561199f9b9eae35c1bbc6d3d027d22dcb83
Henry Dearn <dearn.henry@gmail.com>
2024-08-19 18:33:59 +0100

    use new build function in jsx

--- packages/jsx/build.ts
+++ packages/jsx/build.ts
@@ -1,12 +1,3 @@
-import * as esbuild from 'esbuild'
-import packageJson from './package.json' with {type: "json"}
-import { generateBuildConfig } from '../../build.js'
+import { buildPackage } from '@hd-web/build'
 
-const dependencies = Object.keys(packageJson.devDependencies)
-
-await esbuild.build(
-  generateBuildConfig(dependencies, {
-    entryPoints: ['src/index.ts'],
-    outfile: 'dist/index.js'
-  })
-)
+buildPackage(['./package.json', '../../package.json'])

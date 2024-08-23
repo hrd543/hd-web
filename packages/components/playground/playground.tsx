0b8044c90638b632150059bc8a6c60421fee947b
Henry Dearn <dearn.henry@gmail.com>
2024-08-23 16:55:49 +0100

    improve playground

--- packages/components/playground/playground.tsx
+++ packages/components/playground/playground.tsx
@@ -1,5 +1,37 @@
 import { JSX } from '@hd-web/jsx'
+import { ToastEvent, ToastProvider, WebComponent } from '../dist/index.js'
 
-const App: JSX.Component = () => <div>hi</div>
+class Button extends WebComponent {
+  static get _key() {
+    return 'my-button' as const
+  }
+
+  handleEvent() {
+    console.log('clicking')
+    document.dispatchEvent(
+      new ToastEvent({
+        eventType: 'add',
+        type: 'default',
+        message: 'yoyoyo',
+        duration: 6e3
+      })
+    )
+  }
+
+  connectedCallback() {
+    this.addEventListener('click', this.handleEvent.bind(this))
+  }
+
+  disconnectedCallback() {
+    this.removeEventListener('click', this.handleEvent.bind(this))
+  }
+}
+
+const App: JSX.Component = () => (
+  <>
+    <ToastProvider.key />
+    <Button.key>click me</Button.key>
+  </>
+)
 
 export default App

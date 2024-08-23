7abfcaa2d357c565d5e9487bc8b2c3fad0225f33
Henry Dearn <dearn.henry@gmail.com>
2024-08-23 12:25:52 +0100

    move playground to sub folder

--- packages/components/buildPlayground.ts
+++
@@ -1,7 +0,0 @@
-import { buildSite } from '@hd-web/build'
-
-buildSite(
-  'src/playground/playground.tsx',
-  'src/playground/playgroundBuilt.js',
-  'src/playground/playground.html'
-)
---
+++ packages/components/playground/buildPlayground.ts
@@ -0,0 +1,3 @@
+import { buildSite } from '@hd-web/build'
+
+buildSite('playground.tsx', 'playgroundBuilt.js', 'playground.html')

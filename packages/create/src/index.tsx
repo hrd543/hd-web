0398baa0b24600325d651c0b5ec1fdc1edc8840a
Henry Dearn <dearn.henry@gmail.com>
2024-08-24 16:12:36 +0100

    delete create package for now

--- packages/create/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import { JSX } from '@hd-web/jsx'
-import { Header } from '@hd-web/components'
-
-const App: JSX.Component = () => (
-  <Header items={[{ link: '#about', title: 'About' }]} logo="HD Logs" />
-)
-
-export default App

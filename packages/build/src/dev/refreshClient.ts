8e920de6bc5d29b4defff08848c632828d9b60b1
Henry Dearn <dearn.henry@gmail.com>
2024-09-26 18:42:29 +0100

    remove window check now in memory

--- packages/build/src/dev/refreshClient.ts
+++ packages/build/src/dev/refreshClient.ts
@@ -3,20 +3,10 @@
  * It is responsible for refreshing the page when the server restarts.
  */
 export const getRefreshClientScript = (port: number) => {
-  // Currently this code is run on the node server as well since it's added before
-  // import, and is thus run as a side effect.
-  // Should move to a separate script, or add it after import.
-  // Once done, can remove the typeof check
   return `
-    if (typeof window !== "undefined") {
-      const ws = new WebSocket("ws://localhost:${port}");
-      ws.addEventListener("open", () => {
-        console.log("initialising");
-      })
+    const ws = new WebSocket("ws://localhost:${port}");
       ws.addEventListener("message", () => {
-        console.log("hello");
         window.location.reload()
       });
-    }
   `
 }

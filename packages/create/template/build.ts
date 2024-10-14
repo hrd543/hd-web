fe44bc4f8d538d9aa5f360db4c67c30ae2b168fc
Henry Dearn <dearn.henry@gmail.com>
2024-10-14 17:59:05 +0100

    fix create package

--- packages/create/template/build.ts
+++ packages/create/template/build.ts
@@ -3,4 +3,4 @@ import { buildSite } from '@hd-web/build'
 // This file is run when building your site. These options dictate
 // where your files come from and will be built.
 // Try running npm run build and look inside the build folder.
-buildSite({ entryDir: 'src', outDir: 'build', pageFilename: 'index.tsx' })
+buildSite({ entry: 'src/index.tsx', out: 'build' })
--- packages/create/template/dev.ts
+++ packages/create/template/dev.ts
@@ -3,7 +3,6 @@ import { startDev } from '@hd-web/build'
 // This file is run when developing locally. Try running npm run dev and go to
 // localhost:8080 to see your site!
 startDev({
-  entryDir: 'src',
-  pageFilename: 'index.tsx',
+  entry: 'src/index.tsx',
   port: 8080
 })

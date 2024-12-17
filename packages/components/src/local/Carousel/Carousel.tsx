aacbb811961adae47397feb0ee4ddabb7db32bba
Henry Dearn <dearn.henry@gmail.com>
2024-12-17 16:06:28 +0000

    support resizing

--- packages/components/src/local/Carousel/Carousel.tsx
+++ packages/components/src/local/Carousel/Carousel.tsx
@@ -7,7 +7,11 @@ import { attachIdToElement } from '@hd-web/components'
 
 export type CarouselProps = {
   items: JSX.Element[]
-  visibleCount: number
+  /** The number of slides visible at each breakpoint */
+  breakpoints: Array<{
+    width: number
+    count: number
+  }>
   label: string
   prevIcon: JSX.Element
   nextIcon: JSX.Element
@@ -22,10 +26,10 @@ export const Carousel: JSX.FuncComponent<CarouselProps> = ({
   label,
   prevIcon,
   nextIcon,
-  visibleCount,
+  breakpoints,
   gap = 'var(--sp-400)'
 }) => {
-  const id = interact(carousel, { count: visibleCount, gap })
+  const id = interact(carousel, { breakpoints, itemCount: items.length, gap })
 
   return (
     <div
@@ -45,13 +49,7 @@ export const Carousel: JSX.FuncComponent<CarouselProps> = ({
       <div class="hd-carousel__main">
         <div
           class="hd-carousel__items"
-          style={{
-            'grid-auto-columns':
-              visibleCount === 1
-                ? '100%'
-                : `calc((100% - ${visibleCount - 1} * ${gap}) / ${visibleCount})`,
-            gap
-          }}
+          style={{ gap }}
           aria-live="polite"
           id="hd-carousel-items"
         >
@@ -79,3 +77,25 @@ export const Carousel: JSX.FuncComponent<CarouselProps> = ({
     </div>
   )
 }
+
+/*
+
+There are two interactions we need to support
+1. Clicking the next / prev buttons:
+  - If we click next, we add one to the active index, or set it to 0
+    if we're on the last slide.
+  - If we click prev, we subtract one from the active index, or set
+    it to the last slide if we're on 0
+
+2. Resizing the window
+  - Each time we resize, we need to change the number of cards which are visible
+    at once, and make sure the activeIndex still points to something.
+
+We have two pieces of reactive state:
+- itemsPerSlide
+- activeIndex
+
+
+
+
+*/
--- packages/components/src/local/Carousel/carouselInteract.ts
+++ packages/components/src/local/Carousel/carouselInteract.ts
@@ -1,33 +1,91 @@
 import { getContainerElement } from '../../shared/getContainerElement.js'
 
 type HdCarouselProps = {
-  count: number
+  breakpoints: Array<{
+    width: number
+    count: number
+  }>
+  itemCount: number
   gap: string
 }
 
-export const carousel = (id: number, props: HdCarouselProps) => {
+const getSlideCount = (itemCount: number, itemsPerSlide: number) =>
+  Math.ceil(itemCount / itemsPerSlide)
+
+export const carousel = (
+  id: number,
+  { breakpoints, itemCount, gap }: HdCarouselProps
+) => {
   const container = getContainerElement(id, 'carousel')
   let activeIndex = 0
+  let itemsPerSlide = 0
 
   const items = container.querySelector('.hd-carousel__items') as HTMLDivElement
 
-  const setTransform = () => {
+  const updateActiveIndex = (index: number) => {
+    if (index === activeIndex) {
+      return
+    }
+
+    activeIndex = index
     items.style.transform =
       activeIndex === 0
         ? ''
-        : `translateX(calc(-${100 * activeIndex}% - ${activeIndex} * ${props.gap}))`
+        : `translateX(calc(-${100 * activeIndex}% - ${activeIndex} * ${gap}))`
   }
 
   const prev = () => {
-    activeIndex = activeIndex === 0 ? props.count - 1 : activeIndex - 1
-    setTransform()
+    updateActiveIndex(
+      activeIndex === 0
+        ? getSlideCount(itemCount, itemsPerSlide) - 1
+        : activeIndex - 1
+    )
   }
 
   const next = () => {
-    activeIndex = activeIndex === props.count - 1 ? 0 : activeIndex + 1
-    setTransform()
+    updateActiveIndex(
+      activeIndex === getSlideCount(itemCount, itemsPerSlide) - 1
+        ? 0
+        : activeIndex + 1
+    )
   }
 
   container.querySelector('.hd-carousel__prev')?.addEventListener('click', prev)
   container.querySelector('.hd-carousel__next')?.addEventListener('click', next)
+
+  const updateItemsPerSlide = (newCount: number) => {
+    if (itemsPerSlide === newCount) {
+      return
+    }
+
+    itemsPerSlide = newCount
+    items.style.gridAutoColumns =
+      itemsPerSlide === 1
+        ? '100%'
+        : `calc((100% - ${itemsPerSlide - 1} * ${gap}) / ${itemsPerSlide})`
+    const slideCount = getSlideCount(itemCount, newCount)
+    updateActiveIndex(Math.min(activeIndex, slideCount - 1))
+  }
+
+  const sortedBreakpoints = [...breakpoints].sort((a, b) => a.width - b.width)
+  const handleResize = () => {
+    let match = 0
+    for (const b of sortedBreakpoints) {
+      if (b.width <= window.innerWidth) {
+        match = b.count
+      }
+    }
+
+    if (match === 0) {
+      throw new Error(
+        `Width of ${window.innerWidth} didn't match any breakpoints.`
+      )
+    }
+
+    updateItemsPerSlide(match)
+  }
+
+  window.addEventListener('resize', handleResize)
+  // Make sure we display the right number initially.
+  handleResize()
 }

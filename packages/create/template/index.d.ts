3cfd65e9018c1db1b98197018d8c2cdbce5b381d
Henry Dearn <dearn.henry@gmail.com>
2024-12-20 20:52:29 +0000

    allow importin of images as module

---
+++ packages/create/template/index.d.ts
@@ -0,0 +1,17 @@
+declare module '*.jpg' {
+  declare const path: string
+
+  export default path
+}
+
+declare module '*.png' {
+  declare const path: string
+
+  export default path
+}
+
+declare module '*.webp' {
+  declare const path: string
+
+  export default path
+}

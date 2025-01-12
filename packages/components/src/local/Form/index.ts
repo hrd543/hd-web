4cab4a74fbdd5ea87af166a0428314b690c7ad6d
Henry Dearn <dearn.henry@gmail.com>
2025-01-12 14:10:21 +0000

    export clear functon

--- packages/components/src/local/Form/index.ts
+++ packages/components/src/local/Form/index.ts
@@ -1,2 +1,3 @@
 export { usePostForm, type UsePostFormProps } from './usePostForm.js'
+export { useClearFormOnSuccess } from './useClearFormOnSuccess.js'
 export { HdFormEvent, hdFormEventKey } from './events.js'

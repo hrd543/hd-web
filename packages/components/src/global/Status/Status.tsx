1f492be21a0dd14c5c3dc68e7dabba55707bc874
Henry Dearn <dearn.henry@gmail.com>
2025-01-13 21:47:52 +0000

    add status component with styles and variables

---
+++ packages/components/src/global/Status/Status.tsx
@@ -0,0 +1,12 @@
+import './Status.css'
+
+import { type JSX } from '@hd-web/jsx'
+
+export type StatusProps = {
+  type: 'success' | 'error'
+  message: string
+}
+
+export const Status: JSX.FuncComponent<StatusProps> = ({ type, message }) => {
+  return <div class={`hd-status hd-status--${type}`}>{message}</div>
+}

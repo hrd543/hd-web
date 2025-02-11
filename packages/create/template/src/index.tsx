678b4902cf131db97cc39a80c1203c433f946e51
Henry Dearn <dearn.henry@gmail.com>
2025-02-11 19:07:22 +0000

    update create template to use new stuff

--- packages/create/template/src/index.tsx
+++ packages/create/template/src/index.tsx
@@ -1,10 +1,22 @@
-import { Page } from '@hd-web/build'
 import './defaults.css'
 import './variables.css'
+import './utils.css'
+import { SiteFunction } from '@hd-web/build'
+import { Meta } from '@hd-web/components/head'
+import { NotFound } from '@hd-web/components/global'
 
 // This is your page's html
-const App: Page = () => ({
-  body: <div>Hello world!</div>
+const App: SiteFunction = () => ({
+  body: <div>Hello world!</div>,
+  title: 'My new site!',
+  description: 'My awesome new site made with hd-web',
+  head: <Meta />,
+  routes: {
+    404: () => ({
+      title: 'Not found',
+      body: <NotFound />
+    })
+  }
 })
 
 // It needs to be the default export in order to build

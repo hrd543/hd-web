fcb3c502c2c24de727af7028811fa408448b8f62
Henry Dearn <dearn.henry@gmail.com>
2025-08-05 20:41:00 +0100

    remove temp play folder

--- packages/components/src/play/build.ts
+++
@@ -1,9 +0,0 @@
-import { defaultEsbuildOptions, hdWebPlugin } from '@hd-web/esbuild-plugin'
-import * as esbuild from 'esbuild'
-
-await esbuild.build({
-  ...defaultEsbuildOptions,
-  minify: true,
-  bundle: true,
-  plugins: [hdWebPlugin({ entry: './App2.tsx', out: 'build2' })]
-})

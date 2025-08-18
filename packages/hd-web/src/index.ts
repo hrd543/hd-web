bb8afe31992f0d7915578b8844d52425a59858a4
Henry Dearn <dearn.henry@gmail.com>
2025-08-18 17:36:43 +0100

    clean up package jsons

--- packages/hd-web/src/index.ts
+++ packages/hd-web/src/index.ts
@@ -2,3 +2,13 @@ export { build, type HdBuildConfig } from './build/index.js'
 export { Component, registerClient } from './client/index.js'
 export { dev, type HdDevConfig } from './dev/index.js'
 export * from './types/index.js'
+export {
+  type IComponent,
+  type HdElement,
+  type HdNode,
+  type BaseProps,
+  type Props,
+  type FuncComponent,
+  type ClientProps,
+  type Html
+} from '@hd-web/jsx'

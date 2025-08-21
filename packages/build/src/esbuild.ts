496810fb480d3f5ecf8e4ffd1e95a299ecc2796b
Henry Dearn <dearn.henry@gmail.com>
2025-08-21 16:29:51 +0100

    delete old packages

--- packages/build/src/esbuild.ts
+++
@@ -1,4 +0,0 @@
-import * as Build from './build.js'
-import { BuildSiteConfig } from './config.js'
-
-export const build = async (rawConfig: Partial<BuildSiteConfig> = {}) => {}
--- packages/esbuild-plugin/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { plugin as hdWebPlugin } from './plugin.js'
-export * from './defaults.js'
-export { build } from './build.js'
-export { dev } from './dev.js'

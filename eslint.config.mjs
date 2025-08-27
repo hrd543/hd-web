f9fd953273d3f56a25bf6a9e93a8b515f7061940
Henry Dearn <dearn.henry@gmail.com>
2025-08-27 20:05:39 +0100

    try new eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,19 @@
 import eslint from '@eslint/js'
+import sort from 'eslint-plugin-simple-import-sort'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: {
+      'simple-import-sort': sort
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
+    }
+  },
   {
     ignores: [
       '**/node_modules/',
@@ -16,7 +26,9 @@ export default tseslint.config(
     rules: {
       // This is used to type custom elements with jsx
       '@typescript-eslint/no-namespace': 'off',
-      '@typescript-eslint/no-explicit-any': 'off'
+      '@typescript-eslint/no-explicit-any': 'off',
+      'import/order': 'off',
+      'sort-imports': 'off'
     }
   }
 )

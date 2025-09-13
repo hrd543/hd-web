54cd0132519a39a88932d2bb1d03fdbe9906674d
Henry Dearn <dearn.henry@gmail.com>
2025-09-13 18:09:59 +0100

    remove erroneous comment

--- packages/hd-web/src/dev/dev.ts
+++ packages/hd-web/src/dev/dev.ts
@@ -8,7 +8,6 @@ import { getServeHtml } from './serveHtml.js'
 export const dev = async (config: Partial<DevConfig> = {}) => {
   const fullConfig = validateConfig(config)
   const app = express()
-  console.log('running')
 
   const server = await createServer({
     root: process.cwd(),
